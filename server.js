57b8cca8fe8e8aae26b75593207e375607a7d450
Jack <nian.jack@gmail.com>
2019-06-05 07:43:38 -0400

    fixed path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { routes } = require('./app');
+const { routes } = require('./src/app');
 const { createTerminus } = require('@godaddy/terminus');
 
 
