879ff7803258889ea06c88dcdb26d89034d44878
jack <nian.jack@gmail.com>
2019-06-28 16:44:11 +0000

    added component directory to prep script

--- preproc.sh
+++ preproc.sh
@@ -10,24 +10,29 @@ then
 fi
 server=$(echo "$server" | sed 's/\./\\\./g')
 
-cd /var/www/frontend-integration//src/pages/SignUpPanel
+cd /var/www/frontend-integration/src/pages/SignUpPanel
 sed -i "s|http://localhost:8080|$server|g" *.js
 sed -i "s|http://localhost:3000|$server|g" *.js
 sed -i "s|http://localhost:3001|$server|g" *.js
 sed -i "s|http://138.197.175.92|$server|g" *.js
 
-cd /var/www/frontend-integration//src/pages/LoginPanel
+cd /var/www/frontend-integration/src/pages/LoginPanel
 sed -i "s|http://localhost:8080|$server|g" *.js
 sed -i "s|http://localhost:3000|$server|g" *.js
 sed -i "s|http://localhost:3001|$server|g" *.js
 sed -i "s|http://138.197.175.92|$server|g" *.js
 
-cd /var/www/frontend-integration//src/pages/Forgot
+cd /var/www/frontend-integration/src/pages/Forgot
 sed -i "s|http://localhost:8080|$server|g" *.js
 sed -i "s|http://localhost:3000|$server|g" *.js
 sed -i "s|http://localhost:3001|$server|g" *.js
 sed -i "s|http://138.197.175.92|$server|g" *.js
 
+cd /var/www/frontend-integration/src/components
+sed -i "s|http://localhost:8080|$server|g" *.js
+sed -i "s|http://localhost:3000|$server|g" *.js
+sed -i "s|http://localhost:3001|$server|g" *.js
+sed -i "s|http://138.197.175.92|$server|g" *.js
 
 cd /var/www/frontend-integration/src/app/frontend
 sed -i "s|http://localhost:8080|$server|g" *.js
